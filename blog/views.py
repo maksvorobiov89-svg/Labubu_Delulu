0e1e1c4f31b930adebaf1e9b597ac519f8767036
Maks <maksvorobiov89@gmail.com>
2026-04-13 22:17:59 +0300

    Робоча версія

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    # Дістаємо всі пости з бази даних, сортуємо від найновіших до найстаріших
+    posts = Post.objects.all().order_by('-pub_date')
+    # Передаємо їх у HTML-шаблон
+    return render(request, 'blog/post_list.html', {'posts': posts})
