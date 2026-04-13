c31c4c26e7f422cc828925aa8667b7293f4cd5d3
Maks <maksvorobiov89@gmail.com>
2026-04-13 23:14:37 +0300

    Вирішив пару багів)

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
     content = models.TextField(verbose_name="Зміст")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор")
+    author = models.CharField(max_length=100, verbose_name="Автор", default="Анонім")
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name="Дата публікації")
     category = models.CharField(max_length=100, verbose_name="Категорія")
 
